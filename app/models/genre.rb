794ac41cba7369d9f4c1a9843b6715064e637087
Jesse Wenger <wengerj@gmail.com>
2021-03-30 01:23:49 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.all.names
+    self.artists.all.name
   end
 end
