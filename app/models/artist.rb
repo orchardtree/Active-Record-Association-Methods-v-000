794d4dfb8547955cf804807a4d135a66cfebfe96
Jesse Wenger <wengerj@gmail.com>
2021-03-30 00:50:52 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
-    self.song.first
+    self.whereself.song.first
   end
 
   def song_count
